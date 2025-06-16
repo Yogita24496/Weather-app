3eef15a616423ddf7d414e78e5cc010a03777fb7
Yogita Patil <yogita.patil@hurix.com>
2025-06-16 16:51:32 +0530

    Added code for about page

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>This is a weather forecasting app built using React.</p>
+    </div>
+  );
+};
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-
 import './App.css';
 import WeatherApp from './WeatherApp';
+import About from './About';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <WeatherApp />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Weather</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<WeatherApp />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
